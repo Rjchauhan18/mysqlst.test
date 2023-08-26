bd8ed12e8fae4ed47d6646a44c31548e43b710ff
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-08-26 23:32:42 +0530

    done

---
+++ app.py
@@ -0,0 +1,14 @@
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.experimental_connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from users;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(row)
+    # st.write(f"{row.name} has a :{row.pet}:")
