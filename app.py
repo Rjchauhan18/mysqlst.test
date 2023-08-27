a31bd429a91bdceb5f5968a8bba23a5d9b583cf8
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-08-27 09:48:13 +0530

    mysqldb://Rjchauhan:Rjchauhan@18@192.168.0.134:330

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ conn = st.experimental_connection('mysql', type='sql')
 # Perform query.
 df = conn.query('SELECT * from users;', ttl=600)
 
-# Print results.
 for row in df.itertuples():
     st.write(row)
     # st.write(f"{row.name} has a :{row.pet}:")
