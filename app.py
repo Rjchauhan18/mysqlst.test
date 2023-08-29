aa500f37e9b330cc002d30c24ddf9137bb258a62
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-08-29 16:09:16 +0530

    c

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with st.expander("See the data"):
 def SignUp():
     with st.form(key="SignUp"):
         user=st.text_input("Enter Yout Username")
-        Fullname=st.text_input("Enter Yout Full anme")
+        Fullname=st.text_input("Enter Yout Full name")
         email=st.text_input("Enter Yout Email ID")
         password=st.text_input("Enter Yout Password",type="password")
         re_password=st.text_input("Re-Enter Yout Password",type="password")
@@ -19,7 +19,6 @@ def SignUp():
                 db.insert_user(user,Fullname,email,password)
                 st.info("User successfully inserted")
                 st.balloons()
-
 
 with st.container():
     login,signup=st.tabs(["Login", "SignUp"])
