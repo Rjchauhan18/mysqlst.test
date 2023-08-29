1cd7aec5bb08fd8f5da0989b974f61f03f9279f6
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-08-29 13:51:47 +0530

    def insert_user(username,email,password):     date_join=str(datetime.datetime.now())     return db.put(username,email,password)

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 # streamlit_app.py
 
 import streamlit as st
+from deta import Deta
+import datetime
+Deta_key ="d0tfyvz8ygw_Q3L9xn7PkZmmCQQzhFK9atpNn5TA5iG5"
 
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
+deta= Deta(Deta_key)
 
-# Perform query.
-df = conn.query('SELECT * from users;', ttl=600)
 
-for row in df.itertuples():
-    st.write(row)
-    # st.write(f"{row.name} has a :{row.pet}:")
+db=deta.Base("auth")
+
+def insert_user(username,email,password):
+    date_join=str(datetime.datetime.now()) 
+    return db.put(username,email,password,date_join)
+
+
+user=st.text_input("Enter Yout username")
+email=st.text_input("Enter Yout Email ID")
+password=st.text_input("Enter Yout Password",type="password")
+
+if st.button("submit"):
+    insert_user(user,email,password)
+    st.info("User successfully inserted")
+    st.balloons()
+# # Initialize connection.
+# conn = st.experimental_connection('mysql', type='sql')
+
+# # Perform query.
+# df = conn.query('SELECT * from users;', ttl=600)
+
+# for row in df.itertuples():
+#     st.write(row)
+#     # st.write(f"{row.name} has a :{row.pet}:")
