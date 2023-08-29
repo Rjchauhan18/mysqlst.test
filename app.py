a125aaaf3be380253d14b8f725749afacf9e2e21
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-08-29 18:51:28 +0530

    db

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 import streamlit as st
-# import db
-import os
-from deta import Deta
-from dotenv import load_dotenv
-import datetime
+import db
+# import os
+# from deta import Deta
+# from dotenv import load_dotenv
+# import datetime
 
-load_dotenv(".env")
-DETA_KEY =os.getenv("DETA_KEY")
+# load_dotenv(".env")
+# DETA_KEY =os.getenv("DETA_KEY")
 
-# st.write(DETA_KEY)
-# print(DETA_KEY)
-# st.write(DETA_KEY)
-deta= Deta(DETA_KEY)
-
-
-db=deta.Base("auth")
-
-def insert_user(username,full_name,email,password):
-    date_join=str(datetime.datetime.now()) 
-    db.put({"key":email,"Usename":username,"Fullname":full_name,"Password":password,"Date of join":date_join})
+# # st.write(DETA_KEY)
+# # print(DETA_KEY)
+# # st.write(DETA_KEY)
+# deta= Deta(DETA_KEY)
 
 
-def fetch_user():
-    users=db.fetch()
-    return users.items
+# db=deta.Base("auth")
 
-d=fetch_user()
+# def insert_user(username,full_name,email,password):
+#     date_join=str(datetime.datetime.now()) 
+#     db.put({"key":email,"Usename":username,"Fullname":full_name,"Password":password,"Date of join":date_join})
+
+
+# def fetch_user():
+#     users=db.fetch()
+#     return users.items
+
+d=db.fetch_user()
 with st.expander("See the data"):
     st.write(d)
 
@@ -41,7 +41,7 @@ def SignUp():
         if st.form_submit_button("Submit"):
             if password==re_password:
                         
-                insert_user(user,Fullname,email,password)
+                db.insert_user(user,Fullname,email,password)
                 st.info("User successfully inserted")
                 st.balloons()
 
